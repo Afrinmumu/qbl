edc14afdf5a9c570b5cbcfa097fdf066b1c50d25
Sadia <52107024+Afrinmumu@users.noreply.github.com>
2019-11-26 22:19:20 +0600

    make responsive

--- homepage.php
+++ homepage.php
@@ -6,13 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="style2.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+            $('.menu').click(function(){
+              $('.ul').menuClass('active');  
+
+            })
+
+        })
+        </script>
+     
 </head>
 <body>
     <section>
     <header>
     <a href="#" class="uap"><img src="photos/uap.png" width="120" height="110"></a>
     <a href="#" class="menu">Menu</a>
-    <ul>
+    <ul class="">
     <li><a href="#">Home</a></li> 
     <li><a href="#">Course</a></li> 
     <li><a href="#">Resource</a></li> 
