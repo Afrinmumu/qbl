1ae226af4e1fda3dc717913738d48cb092ffc900
Sadia <52107024+Afrinmumu@users.noreply.github.com>
2019-11-26 21:40:14 +0600

    home-page font design

---
+++ homepage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+    <section>
+    <header>
+    <a href="#" class="uap"><img src="photos/uap.png" width="120" height="110"></a>
+    <a href="#" class="menu">Menu</a>
+    <ul>
+    <li><a href="#">Home</a></li> 
+    <li><a href="#">Course</a></li> 
+    <li><a href="#">Resource</a></li> 
+    <li><a href="#">Book</a></li> 
+    <li><a href="#">Upload</a></li> 
+    </ul>
+    </header>
+    </section>
+</body>
+</html>
